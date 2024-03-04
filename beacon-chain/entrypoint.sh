58461e86c736b58182676eb009e2b767c8ba4dfe
dappnodedev <144998261+dappnodedev@users.noreply.github.com>
2024-03-04 17:39:54 +0100

    Improve package build (#155)
    * Update workflow actions versions
    
    * Improve full package
    
    * Use slim version of debian for build stage

--- beacon-chain/entrypoint.sh
+++ beacon-chain/entrypoint.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-if [[ -n $CHECKPOINT_SYNC_URL ]]; then
+if [ -n "$CHECKPOINT_SYNC_URL" ]; then
   EXTRA_OPTS="--checkpoint-sync-url=${CHECKPOINT_SYNC_URL} --genesis-beacon-api-url=${CHECKPOINT_SYNC_URL} ${EXTRA_OPTS}"
 else
-  EXTRA_OPTS="--genesis-state=/genesis.ssz ${EXTRA_OPTS}"
+  EXTRA_OPTS="--genesis-state=${GENESIS_FILE_PATH} ${EXTRA_OPTS}"
 fi
 
 case $_DAPPNODE_GLOBAL_EXECUTION_CLIENT_PRATER in
@@ -25,33 +25,25 @@ case $_DAPPNODE_GLOBAL_EXECUTION_CLIENT_PRATER in
   ;;
 esac
 
-# MEVBOOST: https://hackmd.io/@prysmaticlabs/BJeinxFsq
-if [ -n "$_DAPPNODE_GLOBAL_MEVBOOST_PRATER" ] && [ "$_DAPPNODE_GLOBAL_MEVBOOST_PRATER" == "true" ]; then
+if [ -n "$_DAPPNODE_GLOBAL_MEVBOOST_PRATER" ] && [ "$_DAPPNODE_GLOBAL_MEVBOOST_PRATER" = "true" ]; then
   echo "MEVBOOST is enabled"
   MEVBOOST_URL="http://mev-boost.mev-boost-goerli.dappnode:18550"
-  if curl --retry 5 --retry-delay 5 --retry-all-errors "${MEVBOOST_URL}"; then
-    EXTRA_OPTS="--http-mev-relay=${MEVBOOST_URL} ${EXTRA_OPTS}"
-  else
-    echo "MEVBOOST is enabled but ${MEVBOOST_URL} is not reachable"
-    curl -X POST -G 'http://my.dappnode/notification-send' --data-urlencode 'type=danger' --data-urlencode title="${MEVBOOST_URL} is not available" --data-urlencode 'body=Make sure the mevboost is available and running'
-  fi
+  EXTRA_OPTS="--http-mev-relay=${MEVBOOST_URL} ${EXTRA_OPTS}"
 fi
 
-# Chek the env FEE_RECIPIENT_ADDRESS has a valid ethereum address if not set to the null address
-if [ -n "$FEE_RECIPIENT_ADDRESS" ] && [[ "$FEE_RECIPIENT_ADDRESS" =~ ^0x[a-fA-F0-9]{40}$ ]]; then
-    echo "FEE_RECIPIENT is valid"
+if [ -n "$FEE_RECIPIENT_ADDRESS" ] && echo "$FEE_RECIPIENT_ADDRESS" | grep -Eq '^0x[a-fA-F0-9]{40}$'; then
+  echo "FEE_RECIPIENT is valid"
 else
-    echo "FEE_RECIPIENT is not a valid ethereum address, setting it to the null address"
-    FEE_RECIPIENT_ADDRESS="0x0000000000000000000000000000000000000000"
+  echo "FEE_RECIPIENT is not a valid ethereum address, setting it to the null address"
+  FEE_RECIPIENT_ADDRESS="0x0000000000000000000000000000000000000000"
 fi
 
-# If EXTRA_OPTS does not include flag --suggested-fee-recipient, append it
-if [[ $EXTRA_OPTS != *"suggested-fee-recipient"* ]]; then
+if ! echo "$EXTRA_OPTS" | grep -q -- "--suggested-fee-recipient"; then
   echo "Adding --suggested-fee-recipient=${FEE_RECIPIENT_ADDRESS} to EXTRA_OPTS"
   EXTRA_OPTS="--suggested-fee-recipient=${FEE_RECIPIENT_ADDRESS} ${EXTRA_OPTS}"
 fi
 
-exec -c beacon-chain \
+exec /beacon-chain \
   --datadir=/data \
   --rpc-host=0.0.0.0 \
   --accept-terms-of-use \
@@ -61,7 +53,7 @@ exec -c beacon-chain \
   --p2p-tcp-port=$P2P_TCP_PORT \
   --p2p-udp-port=$P2P_UDP_PORT \
   --execution-endpoint=$HTTP_ENGINE \
-  --grpc-gateway-port=3500 \
+  --grpc-gateway-port=$VALIDATOR_PORT \
   --grpc-gateway-corsdomain=$CORSDOMAIN \
-  --jwt-secret=/jwtsecret \
+  --jwt-secret=$JWT_PATH \
   $EXTRA_OPTS
--- validator/entrypoint.sh
+++ validator/entrypoint.sh
@@ -1,51 +1,26 @@
-#!/bin/bash
+#!/bin/sh
 
-export NETWORK="prater"
-VALIDATOR_PORT=3500
-export WEB3SIGNER_API="http://web3signer.web3signer-${NETWORK}.dappnode:9000"
-export WALLET_DIR="/root/.eth2validators"
+WEB3SIGNER_API=http://web3signer.web3signer-${NETWORK}.dappnode:9000
+DATA_DIR=/root/.eth2validators
+WALLET_DIR=${DATA_DIR}/prysm-wallet-v2
 
 # Copy auth-token in runtime to the prysm token dir
 mkdir -p ${WALLET_DIR}
 cp /auth-token ${WALLET_DIR}/auth-token
 
-# Migrate if required
-if [[ $(validator accounts list \
-  --wallet-dir="$WALLET_DIR" \
-  --wallet-password-file="${WALLET_DIR}/walletpassword.txt" \
-  --prater \
-  --accept-terms-of-use) ]]; then
-  {
-    echo "found validators, starging migration"
-    eth2-migrate.sh &
-    wait $!
-  }
-else
-  { echo "validators not found, no migration needed"; }
-fi
-
-# Remove manual migration if older than 20 days
-find /root -type d -name manual_migration -mtime +20 -exec rm -rf {} +
-
 # MEVBOOST: https://hackmd.io/@prysmaticlabs/BJeinxFsq
-if [ -n "$_DAPPNODE_GLOBAL_MEVBOOST_PRATER" ] && [ "$_DAPPNODE_GLOBAL_MEVBOOST_PRATER" == "true" ]; then
+if [ -n "$_DAPPNODE_GLOBAL_MEVBOOST_PRATER" ] && [ "$_DAPPNODE_GLOBAL_MEVBOOST_PRATER" = "true" ]; then
   echo "MEVBOOST is enabled"
-  MEVBOOST_URL="http://mev-boost.mev-boost-goerli.dappnode:18550"
-  if curl --retry 5 --retry-delay 5 --retry-all-errors "${MEVBOOST_URL}"; then
-    EXTRA_OPTS="--enable-builder ${EXTRA_OPTS}"
-  else
-    echo "MEVBOOST is enabled but ${MEVBOOST_URL} is not reachable"
-    curl -X POST -G 'http://my.dappnode/notification-send' --data-urlencode 'type=danger' --data-urlencode title="${MEVBOOST_URL} is not available" --data-urlencode 'body=Make sure the mevboost is available and running'
-  fi
+  EXTRA_OPTS="--enable-builder ${EXTRA_OPTS}"
 fi
 
 oLang=$LANG oLcAll=$LC_ALL
-LANG=C LC_ALL=C 
-graffitiString=${GRAFFITI:0:32}
+LANG=C LC_ALL=C
+graffitiString=$(echo ${GRAFFITI} | cut -c 1-32)
 LANG=$oLang LC_ALL=$oLcAll
 
-exec -c validator --prater \
-  --datadir="$WALLET_DIR" \
+exec /validator --prater \
+  --datadir="$DATA_DIR" \
   --wallet-dir="$WALLET_DIR" \
   --monitoring-host 0.0.0.0 \
   --beacon-rpc-provider="$BEACON_RPC_PROVIDER" \
